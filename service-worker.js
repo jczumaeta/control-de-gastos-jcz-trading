5823b0a6fa1a570bc84542b369c0198aa01e31c4
jczumaeta <64164771+jczumaeta@users.noreply.github.com>
2026-09-18 10:45:34 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gastos-cache-v2';
+const CACHE_NAME = 'gastos-cache-v3';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
